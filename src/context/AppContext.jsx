28e927c823deb7b52f32571aad541fd8fc5ba832
Sumit Kumar Baranwal <sumitkb2004@gmail.com>
2025-02-26 04:32:03 +0530

    NavBar  completed

---
+++ src/context/AppContext.jsx
@@ -0,0 +1,18 @@
+import {useState, createContext } from "react";
+export const AppContext = createContext()
+
+const AppContextProvider = (props)=>{
+    const [user, setUser] = useState(null);
+
+    const value = {
+        user, setUser
+    }
+
+    return (
+        <AppContext.Provider value = {value}>
+            {props.children}
+        </AppContext.Provider>
+    )
+}
+
+export default AppContextProvider
