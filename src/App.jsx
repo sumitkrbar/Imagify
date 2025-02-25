c88d5bf5fc8ad8ee11e5204e4d040d7383f2d0a9
Sumit Kumar Baranwal <sumitkb2004@gmail.com>
2025-02-25 20:35:13 +0530

    adding navbar(incomplete)

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import {Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import Result from './pages/Result'
 import BuyCredit from './pages/BuyCredit'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div>
+    <div className='px-4 sm:px-10 md:px-14 lg:px-28 
+    min-h-screen bg-gradient-to-b frm-teal-50 
+    to-orange-50'>
+    <Navbar/>
       <Routes>
         <Route path = '/' element={<Home/>}/>
         <Route path = '/result' element={<Result/>}/>
         <Route path = '/buy' element={<BuyCredit/>}/>
-        
+        <Route path = '/home' element={<Home/>}/>
       </Routes>
       
     </div>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import {assets} from "../assets/assets"
+import { Link, useNavigate } from 'react-router-dom'
+const Navbar = () => {
+    const [user, setUser] = useState(null);
+    const navigate = useNavigate();
+  return (
+    <div className='flex items-center justify-between py-4'>
+        <Link to='/'>
+        <img src={assets.logo} alt="" className='w-28
+        sm:w-32 lg:w-40' />
+        </Link>
+
+        <div>
+            {
+            user ? 
+            <div></div>
+            : 
+            <div className='flex items-center gap-2 sm:gap-5'>
+                <p onClick={()=>navigate('/buy')} className='cursor-pointer'>Pricing</p>
+                <button className='bg-zinc-800 text-white px-7 py-2
+                 sm:px-7 text-sm rounded-full'>Login</button>
+            </div>
+            }
+            
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
