b7a09511694f3bfb034a4ed66a4547c5941eaeac
Sumit Kumar Baranwal <sumitkb2004@gmail.com>
2025-02-28 03:20:41 +0530

    Added animation in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { assets } from '../assets/assets'
 import {motion} from "motion/react"
+import {AppContext} from '../context/AppContext'
+import {useNavigate} from 'react-router-dom'
 const Header = () => {
+
+    const {user, setShowLogin} = useContext(AppContext);
+    const navigate = useNavigate();
+
+    const onClickHandler = ()=>{
+        if(user){
+            navigate('/result')
+        }
+        else{
+            setShowLogin(true);
+        }
+    }
   return (
     <motion.div className='flex flex-col justify-center items-center
     text-center my-20'
@@ -20,25 +34,53 @@ const Header = () => {
             <img src={assets.star_icon} alt="" />
         </motion.div>
         <h1 className='text-4xl max-w-[300px] sm:text-7xl 
-        sm:max-w-[590px] mx-auto mt-10 text-center'>Turn text to <span className='text-blue-600'>image</span>, in seconds!</h1>
-        <p className='text-center max-w-xl mx-auto mt-5'>
+        sm:max-w-[590px] mx-auto mt-10 text-center'>Turn text to <br/>
+        <span className='text-blue-600'
+        initial={{opacity: 0}}
+        animate={{opacity:1}}
+        transition={{delay: 0.4, duration: 2}}
+        >image</span>, in seconds!</h1>
+        <motion.p className='text-center max-w-xl mx-auto mt-5'
+        initial ={{opacity:0, y:20}} 
+        animate={{opacity: 1, y: 0}} 
+        transition={{dealy:0.6, duration:0.8}}
+        >
             Unleash your creativity with AI. Turn your imagination
             into visual art in seconds - just type and watch the magic 
             happen.
-        </p>
-        <button className='sm:text-lg text-white bg-black w-auto
-        mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full'>
+        </motion.p>
+        <motion.button onClick={onClickHandler} className='sm:text-lg text-white bg-black w-auto
+        mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full'
+        whileHover={{scale: 1.05}}
+        whileTap={{scale: 0.95}}
+        initial={{opacity: 0}}
+        animate={{opacity: 1}}
+        transition={{default: {duration: 0.5}, opacity: {delete:
+            0.8, duration: 1}}}
+        >
             Generate Images 
             <img className='h-6' src={assets.star_group} alt="" />
-        </button>
-        <div className='flex flex-wrap justify-center mt-16 gap-3'>
+        </motion.button>
+        <motion.div 
+        initial ={{opacity: 0}} 
+        animate={{opacity: 1}} 
+        transition={{dealy: 1, duration: 1}}
+        className='flex flex-wrap justify-center mt-16 gap-3'>
             {Array(6).fill('').map((item, index)=>(
-                <img className='rounded hover:scale-105 transition-all
+                <motion.img 
+                whileHover={{scale: 1.05, duration: 0.1}}
+                className='rounded hover:scale-105 transition-all
                 duration-300 cursor-pointer max-sm:w-10' 
                 src={index%2===0 ? assets.sample_img_1 :  assets.sample_img_2} key={index} width={70}/>
             ))}
-        </div>
-        <p className='mt-2 text-neutral-600'>Generated images from Imagify</p>
+        </motion.div>
+
+
+        <motion.p 
+        initial ={{opacity: 0}} 
+        animate={{opacity: 1}} 
+        transition={{dealy:1.2, duration:0.8}}
+        className='mt-2 text-neutral-600'>Generated images from Imagify</motion.p>
     </motion.div>
   )
 }
