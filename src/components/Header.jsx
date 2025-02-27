f5909cf7834b049f09888fe4a522ce5daa85099a
Sumit Kumar Baranwal <sumitkb2004@gmail.com>
2025-02-27 20:49:58 +0530

    Began animating

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import { assets } from '../assets/assets'
-
+import {motion} from "motion/react"
 const Header = () => {
   return (
-    <div className='flex flex-col justify-center items-center
-    text-center my-20'>
-        <div className='text-stone-500 inline-flex text-center gap-2
-        bg-white px-6 py-1 rounded-full border border-neutral-500'> 
+    <motion.div className='flex flex-col justify-center items-center
+    text-center my-20'
+    initial ={{opacity:0.2, y:100}} 
+    transition={{duration:1}} 
+    whileInView={{opacity: 1, y: 0}} 
+    viewport={{once: true}}
+    >
+        <motion.div className='text-stone-500 inline-flex text-center gap-2
+        bg-white px-6 py-1 rounded-full border border-neutral-500'
+        initial ={{opacity:0, y:-20}} 
+        animate={{opacity: 1, y: 0}} 
+        transition={{dealy:0.2, duration:0.8}}
+        > 
             <p>Best text to image generator</p>
             <img src={assets.star_icon} alt="" />
-        </div>
+        </motion.div>
         <h1 className='text-4xl max-w-[300px] sm:text-7xl 
         sm:max-w-[590px] mx-auto mt-10 text-center'>Turn text to <span className='text-blue-600'>image</span>, in seconds!</h1>
         <p className='text-center max-w-xl mx-auto mt-5'>
@@ -30,7 +39,7 @@ const Header = () => {
             ))}
         </div>
         <p className='mt-2 text-neutral-600'>Generated images from Imagify</p>
-    </div>
+    </motion.div>
   )
 }
 
