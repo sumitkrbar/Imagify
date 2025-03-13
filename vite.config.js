a342ea16b782b3944d9faba6cbd2bdab74bc739c
sumitkrbar <169034270+sumitkrbar@users.noreply.github.com>
2025-03-13 18:19:43 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  port: process.env.PORT || 5173,
+  server: {
+    port: process.env.PORT || 5173,
     host: '0.0.0.0'
-})
+  },
+  preview: {
+    port: process.env.PORT || 10000,
+    host: '0.0.0.0',
+    allowedHosts: ['imagify-by-sumit.onrender.com']
+  }
+});
