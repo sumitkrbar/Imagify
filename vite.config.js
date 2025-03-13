eadedbd093dafc5a5364747776d4d7509e7e80b9
sumitkrbar <169034270+sumitkrbar@users.noreply.github.com>
2025-03-13 18:15:08 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  port: process.env.PORT || 5173,
+    host: '0.0.0.0'
 })
